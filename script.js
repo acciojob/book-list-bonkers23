834731afd364bbafb14f3b831dabbf16cb54b8d3
Shariq Hashmi <127607558+bonkers23@users.noreply.github.com>
2025-05-05 22:28:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+let form=document.querySelector("form");
+let btn=document.querySelector("#submit"); 
+let table=document.querySelector("table")
+btn.addEventListener("click",(e)=>{
+	e.preventDefault();
+	let title=document.querySelector("#title").value;
+	let author=document.querySelector("#author").value;
+	let isbn=document.querySelector("#isbn").value;	
+	let tbody=document.querySelector("#book-list")
+
+	let tr=document.createElement("tr");
+	let td1=document.createElement("td");
+	td1.innerText=title;
+	let td2=document.createElement("td");
+	td2.innerText=author;
+	let td3=document.createElement("td");
+	td3.innerText=isbn;
+	let td4=document.createElement("td");
+	let del=document.createElement("button");
+	del.innerText = "X";
+	del.className = "delete";
+	del.style.background="red";
+	del.style.color="white";
+	del.style.background="red";
+	td4.append(del);
+	del.addEventListener("click", () => {
+        tr.remove();
+      });
+	tr.append(td1,td2,td3,td4);
+	tbody.append(tr)
+	table.append(tbody);
+	
+})
